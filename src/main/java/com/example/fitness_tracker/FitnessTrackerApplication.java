cd74eb68c2c6619205005887ce5137ad2188302a
luca <luca.sorg911@gmail.com>
2025-10-11 22:28:18 +0200

    h2 connector added, login page

--- src/main/java/com/example/fitness_tracker/FitnessTrackerApplication.java
+++ src/main/java/com/example/fitness_tracker/FitnessTrackerApplication.java
@@ -2,12 +2,19 @@ package com.example.fitness_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.*;
+import java.sql.DriverManager;
+
+
 
 @SpringBootApplication
 public class FitnessTrackerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+        conn.close();
 		SpringApplication.run(FitnessTrackerApplication.class, args);
+
 	}
 
 }
