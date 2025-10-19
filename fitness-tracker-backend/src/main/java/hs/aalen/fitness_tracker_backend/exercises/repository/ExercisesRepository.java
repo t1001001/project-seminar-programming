c2e8b41195452500c9eb573e93c2965f2231737d
t1001001 <tobias1001001@proton.me>
2025-10-19 17:08:28 +0200

    Added exercises repository

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/exercises/repository/ExercisesRepository.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/exercises/repository/ExercisesRepository.java
@@ -1,1 +1,13 @@
 package hs.aalen.fitness_tracker_backend.exercises.repository;
+
+import hs.aalen.fitness_tracker_backend.exercises.model.Exercises;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ExercisesRepository extends JpaRepository<Exercises, UUID> {
+    Optional<Exercises> findByNameIgnoreCase(String name);
+}
