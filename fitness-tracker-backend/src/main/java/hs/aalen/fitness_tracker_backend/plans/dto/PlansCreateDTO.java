20e5c7cf3db6400c784bac6b98103adda9326e42
t1001001 <tobias1001001@proton.me>
2025-10-20 22:40:13 +0200

    Added plans DTOs

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/dto/PlansCreateDTO.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/dto/PlansCreateDTO.java
@@ -1,1 +1,13 @@
 package hs.aalen.fitness_tracker_backend.plans.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class PlansCreateDTO {
+
+    @NotBlank
+    private String name;
+
+    private String description;
+}
--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/dto/PlansResponseDTO.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/dto/PlansResponseDTO.java
@@ -1,1 +1,11 @@
 package hs.aalen.fitness_tracker_backend.plans.dto;
+
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+public class PlansResponseDTO {
+    private UUID id;
+    private String name;
+    private String description;
+}
