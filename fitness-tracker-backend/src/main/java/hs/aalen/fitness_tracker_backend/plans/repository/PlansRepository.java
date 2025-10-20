db64417a194b562f18485ae770b1381ca7d2b56d
t1001001 <tobias1001001@proton.me>
2025-10-20 22:42:58 +0200

    Added plans repository

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/repository/PlansRepository.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/repository/PlansRepository.java
@@ -1,1 +1,13 @@
 package hs.aalen.fitness_tracker_backend.plans.repository;
+
+import hs.aalen.fitness_tracker_backend.plans.model.Plans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PlansRepository extends JpaRepository<Plans, UUID> {
+    Optional<Plans> findByNameIgnoreCase(String name);
+}
