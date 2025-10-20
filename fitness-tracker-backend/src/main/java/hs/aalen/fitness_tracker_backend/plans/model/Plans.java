655bc9a11080199e1457ec7637b31d1d513adc3b
t1001001 <tobias1001001@proton.me>
2025-10-20 22:35:40 +0200

    Added plans model

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/model/Plans.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/model/Plans.java
@@ -1,1 +1,22 @@
 package hs.aalen.fitness_tracker_backend.plans.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+// TODO: ADD "sessions" FIELD
+@Entity
+@Getter
+@Setter
+public class Plans {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+}
