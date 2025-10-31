aa7d436f6a8ad00772acc2a378da4a68157f9365
luca <luca.sorg911@gmail.com>
2025-10-31 21:50:28 +0100

    fixed imports

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/sessions/dto/SessionsUpdateDto.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/sessions/dto/SessionsUpdateDto.java
@@ -1,7 +1,6 @@
 package hs.aalen.fitness_tracker_backend.sessions.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
