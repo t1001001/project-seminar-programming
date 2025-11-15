22dd6cdde55d42ff2f5e3f4d5cbf8c598548ad85
Marc Donauer <mc.donau@gmx.de>
2025-11-15 15:18:06 +0100

    refactor: remove unused home and plans UI components and pages

--- fitness-tracker-frontend/projects/home-lib/src/public-api.ts
+++ fitness-tracker-frontend/projects/home-lib/src/public-api.ts
@@ -5,5 +5,3 @@
 export * from './lib/domain';
 export * from './lib/services/business/home-content.service';
 export * from './lib/services/providers/home-content.provider';
-export * from './lib/ui/home-feature-card/home-feature-card.component';
-export * from './lib/views/home-dashboard/home-dashboard.component';
--- fitness-tracker-frontend/projects/plans-lib/src/public-api.ts
+++ fitness-tracker-frontend/projects/plans-lib/src/public-api.ts
@@ -5,5 +5,3 @@
 export * from './lib/domain';
 export * from './lib/services/business/plan-manager.service';
 export * from './lib/services/providers/plan-api.provider';
-export * from './lib/ui/plan-card/plan-card.component';
-export * from './lib/views/plans-dashboard/plans-dashboard.component';
