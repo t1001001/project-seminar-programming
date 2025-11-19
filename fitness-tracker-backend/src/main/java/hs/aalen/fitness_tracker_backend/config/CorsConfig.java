cc5e5fd4d1fad675c441ca6a68561f7cb4440d88
Marc Donauer <mc.donau@gmx.de>
2025-11-19 18:48:27 +0100

    Fixed Annotations for Backend, CORS and Error Handling

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/config/CorsConfig.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/config/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4020")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
