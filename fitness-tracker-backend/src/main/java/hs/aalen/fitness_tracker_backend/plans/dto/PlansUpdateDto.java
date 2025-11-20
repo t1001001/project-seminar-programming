9a18dcdd8f5728996524b0ab69d87c5734ad44d2
Marc Donauer <mc.donau@gmx.de>
2025-11-20 14:56:06 +0100

    Update backend: Fix PlansUpdateDto and seed additional sessions

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/dto/PlansUpdateDto.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/plans/dto/PlansUpdateDto.java
@@ -2,13 +2,12 @@ package hs.aalen.fitness_tracker_backend.plans.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import java.util.UUID;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 public class PlansUpdateDto {
     @NotBlank
-    private UUID id;
     private String name;
     private String description;
     private List<UUID> sessions;
