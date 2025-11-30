419ad56420ae98103d455dea3190fff9efafcdb4
lukas9800x3D <lukas19.schulz@gmail.com>
2025-11-30 19:49:43 +0100

    Add SessionsOverview wrapper page.

---
+++ fitness-tracker-frontend/src/app/pages/sessions/sessions-overview/sessions-overview.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SessionsOverviewComponent } from 'sessions-lib';
+
+@Component({
+  selector: 'app-sessions-overview',
+  standalone: true,
+  imports: [SessionsOverviewComponent],
+  template: `<lib-sessions-overview />`,
+  styleUrl: './sessions-overview.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SessionsOverview { }
