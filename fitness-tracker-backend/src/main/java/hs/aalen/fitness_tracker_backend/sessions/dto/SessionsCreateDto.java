4e3fe7f674398ab4fd325ddaa9847c0928125059
lukas9800x3D <lukas19.schulz@gmail.com>
2025-12-04 00:41:34 +0100

    Add validation constraints for orderID in SessionsCreateDto and SessionsUpdateDto

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/sessions/dto/SessionsCreateDto.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/sessions/dto/SessionsCreateDto.java
@@ -1,5 +1,7 @@
 package hs.aalen.fitness_tracker_backend.sessions.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -15,5 +17,8 @@ public class SessionsCreateDto {
     @NotBlank
     private String name;
 
+    @NotNull(message = "Order is required")
+    @Min(value = 1, message = "Order must be between 1 and 30")
+    @Max(value = 30, message = "Order must be between 1 and 30")
     private Integer orderID;
 }
