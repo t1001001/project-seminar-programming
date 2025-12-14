09ec45681c15e466a02bfbd86254d763453b24b4
lukas9800x3D <lukas19.schulz@gmail.com>
2025-12-14 04:18:53 +0100

    feat(app): wire workouts pages

--- fitness-tracker-frontend/src/app/app.routes.ts
+++ fitness-tracker-frontend/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { PlansOverview } from './pages/plans/plans-overview/plans-overview';
 import { PlansDetails } from './pages/plans/plans-details/plans-details';
 import { SessionsOverview } from './pages/sessions/sessions-overview/sessions-overview';
 import { SessionsDetails } from './pages/sessions/sessions-details/sessions-details';
+import { WorkoutsOverview } from './pages/workouts/workouts-overview/workouts-overview';
+import { WorkoutsDetails } from './pages/workouts/workouts-details/workouts-details';
 import { HomeLanding } from './pages/home/home-landing/home-landing';
 
 export const routes: Routes = [
@@ -40,6 +42,14 @@ export const routes: Routes = [
   {
     path: 'sessions/:id',
     component: SessionsDetails,
+  },
+  {
+    path: 'workouts',
+    component: WorkoutsOverview,
+  },
+  {
+    path: 'workouts/:id',
+    component: WorkoutsDetails,
   },
   {
     path: '**',
