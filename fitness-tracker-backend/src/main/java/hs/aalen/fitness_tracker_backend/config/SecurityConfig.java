fdb5e74be7430d79b8144e5fd2ccb7666c4de610
Marc Donauer <mc.donau@gmx.de>
2026-01-03 12:40:13 +0100

    feat(backend): enable CORS in SecurityConfig

--- fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/config/SecurityConfig.java
+++ fitness-tracker-backend/src/main/java/hs/aalen/fitness_tracker_backend/config/SecurityConfig.java
@@ -18,15 +18,13 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(sm ->
-                sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .httpBasic(Customizer.withDefaults());
+                .cors(Customizer.withDefaults()) // Enable CORS
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
